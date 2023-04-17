21b6aa80875eced7edbe8c2c61b7c11744617490
Rostik <rostislav.kh@zina.com.ua>
2023-04-17 16:32:36 +0300

    Settings to laravel-mix

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const mix = require('laravel-mix');
+
+//========================================================= JS file ===========================================================
+
+// default
+mix.js('resources/js/default.js', 'js');
+
+//========================================================= SCSS file ===========================================================
+
+
+// default
+mix.sass('resources/scss/style.scss', 'css');
+mix.sass('resources/scss/fonts.scss', 'css');
+mix.sass('resources/scss/variables.scss', 'css');
+
+// lib
+// mix.styles([
+//     'resources/scss/libs/slick.css',
+// ], 'public/css/all-lib.css');
+
+mix.setPublicPath('public')
+mix.version();
